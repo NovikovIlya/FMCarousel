62fb12e75be45f6696aae5ec23a2990f7f5045a6
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-06-21 07:40:12 +0300

    05

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { Button, Carousel } from 'antd'
 import React from 'react'
+import imageOne from '../assets/desktop-image-hero-1.jpg'
+import imageTwo from '../assets/desktop-image-hero-2.jpg'
+import imageThree from '../assets/desktop-image-hero-3.jpg'
+
 
 function Header() {
     const [count, setCount] = useState(1)
     const carouselRef = React.createRef<any>();
     const array = [
-        { title: 'aaa', text: 'asdsad' },
-        { title: 'bb', text: 'xxx' },
-        { title: 'bbzz', text: 'xxbbx' }
+        { title: 'Discover innovative ways to decorate', text: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.' },
+        { title: "We are available all across the globe", text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, were in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today." },
+        { title: 'Manufactured with the best materials', text: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office' }
     ]
 
     const contentStyle: React.CSSProperties = {
@@ -26,17 +30,17 @@ function Header() {
 
     return (
         <>
-            <div className='flex'>
-                <div className='relative w-[70%]'>
+            <div className='flex border-b-2'>
+                <div className='relative w-[65%]'>
                     <Carousel ref={carouselRef} infinite={false} afterChange={onChange}>
                         <div>
-                            <h3 style={contentStyle}>1</h3>
+                            <img className='w-full' src={imageOne} />
                         </div>
                         <div>
-                            <h3 style={contentStyle}>2</h3>
+                            <img className='w-full' src={imageTwo} />
                         </div>
                         <div>
-                            <h3 style={contentStyle}>3</h3>
+                            <img className='w-full' src={imageThree} />
                         </div>
                     </Carousel>
                     <div className='flex  gap-2 absolute top-2 left-2 text-white'>
@@ -47,25 +51,30 @@ function Header() {
                     </div>
                 </div>
 
-                <div className='w-[30%]'>
+                <div className='w-[45%] flex justify-center items-center flex-col relative'>
                     <div>{array.map((item, index) => {
-                        return index + 1 === count ? item.title : null
+                        return index + 1 === count ? <div className='h-[220px] p-10'><div className='text-3xl'>{item.title}</div><div className=' break-all'>{item.text}</div> </div>: null
                     })}</div>
-                    
-                    <Button
+                    <div className='w-full p-10'>Shop now -&gt; </div>
+
+                    <div className='flex flex-row absolute left-0 bottom-0'>
+                    <div
+                        className='bg-black text-white w-[50px] flex justify-center items-center cursor-pointer h-[50px]'
                         onClick={() => {
                             carouselRef.current.prev();
                         }}
                     >
-                        goto prev
-                    </Button>
-                    <Button
+                        &lt;
+                    </div>
+                    <div
+                        className='bg-black text-white w-[50px] flex justify-center items-center cursor-pointer h-[50px]'
                         onClick={() => {
                             carouselRef.current.next();
                         }}
                     >
-                        goto next
-                    </Button>
+                        &gt;
+                    </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import image from '../assets/image-about-dark.jpg'
+import imageTwo from '../assets/image-about-light.jpg'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='flex  mt-[-2px]'>
+      <img src={image} />
+      <div className='flex justify-center items-center flex-col p-5'>
+        <div className='text-3xl text-left w-full'>About our furniture</div>
+        <div>Our multifunctional collection blends design and function to suit your individual taste.
+          Make each room unique, or pick a cohesive theme that best express your interests and what
+          inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+          or anything in between. Product specialists are available to help you create your dream space.</div>
+      </div>
+      <img src={imageTwo} />
+    </div>
   )
 }
 
